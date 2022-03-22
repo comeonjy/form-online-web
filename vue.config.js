c6ddd98b178d02788a73dd7b55eac3bf1a0f7add
comeonjy <jiangyang.me@gmail.com>
2022-03-22 14:09:54 +0800

    deploy:v0.0.1

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,13 @@ module.exports = {
         port: 1024,
         https: false,
         proxy: {
-            '/': {
+            '/api': {
                 target: 'https://api.jiangyang.me',
                 // target: 'http://localhost:8080',
                 ws: true,
                 changOrigin: true,
                 pathRewrite: {
-                    '^/*': ''
+                    '^/api': ''
                 }
             }
         }
